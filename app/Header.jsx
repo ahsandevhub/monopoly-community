9a0de0a1fdb826783ebe193aede2db8e03e4ae8d
ahsandevhub <contact2ahsanhabib@gmail.com>
2025-01-27 12:15:19 +0600

    added metadata and logo

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,4 +1,11 @@
+"use client";
+
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="bg-red-600 text-white py-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center px-4">
@@ -12,9 +19,21 @@ const Header = () => {
           <h1 className="text-2xl font-bold">Monopoly Community</h1>
         </div>
 
+        {/* Menu Icon (Mobile) */}
+        <button
+          className="md:hidden text-white focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <X size={32} /> : <Menu size={32} />}
+        </button>
+
         {/* Navigation */}
-        <nav>
-          <ul className="flex gap-6 text-lg font-medium">
+        <nav
+          className={`absolute md:static top-16 left-0 w-full md:w-auto bg-red-600 md:bg-transparent shadow-md md:shadow-none md:flex ${
+            isOpen ? "block" : "hidden"
+          }`}
+        >
+          <ul className="flex flex-col md:flex-row gap-6 text-lg font-medium md:items-center md:gap-8 p-4 md:p-0">
             <li>
               <a href="/" className="hover:text-green-400 transition">
                 Home
